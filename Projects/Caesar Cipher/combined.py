015a40135acbb330a91a09c8edf7c08f24da790c
legendyen <legendyen1998@gmail.com>
2023-03-21 23:22:44 +0800

    Create combined.py

---
+++ Projects/Caesar Cipher/combined.py
@@ -0,0 +1,21 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(start_text, shift_amount, direction):
+  end_text = ""
+  if direction == 'decode':
+    shift = shift * -1
+  for i in start_text:
+    position = alphabet.index(i)
+    new_position = position + shift_amount
+    if new_position > 25:
+      new_position = new_position -26 
+    elif new_position < 0:
+      new_position = new_postion + 26
+ 
+    end_text = end_text + alphabet[new_position]
+  print(f"The {direction} text is {end_tet}")
+
+caesar()
