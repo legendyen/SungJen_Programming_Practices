16c1002e2ced1e5502a5dbc16dba0c572991cc79
legendyen <legendyen1998@gmail.com>
2023-03-23 23:50:36 +0800

    Create dictionaries.py

---
+++ Projects/Vendor Machine/dictionaries.py
@@ -0,0 +1,38 @@
+MENU = {
+    "hamburger": {
+        "ingredients": {
+            "bread": 2,
+            "tomato": 1,
+            "lettuce": 2,
+            "patty": 2
+        },
+        "cost": 3.0,
+    },
+    "sandwich": {
+        "ingredients": {
+            "bread": 3,
+            "tomato": 1,
+            "lettuce": 2,
+            "cheese": 1
+        },
+        "cost": 2.5,
+    },
+    "hot-dog": {
+        "ingredients": {
+            "bread": 1,
+            "cheese": 2,
+            "patty": 1,
+        },
+        "cost": 1.5,
+    }
+}
+
+profit = 0
+
+resources = {
+    "bread": 30,
+    "tomato": 20,
+    "lettuce": 20,
+    "cheese": 20,
+    "patty": 20
+}
