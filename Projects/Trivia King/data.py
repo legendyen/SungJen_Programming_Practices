463de42b5db8c49b93d83f62291374cc72ffd3db
legendyen <legendyen1998@gmail.com>
2023-03-25 11:46:35 +0800

    Create data.py

---
+++ Projects/Trivia King/data.py
@@ -0,0 +1,71 @@
+question_data = [
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The programming language \"Python\" is based off a modified version of \"JavaScript\".",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The logo for Snapchat is a Bell.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "medium",
+        "question": "\"Windows NT\" is a monolithic kernel.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "medium",
+        "question": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "Ada Lovelace is often considered the first computer programmer.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "In most programming languages, the operator ++ is equivalent to the statement \"+= 1\".",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]},
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The Windows ME operating system was released in the year 2000.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Computers",
+        "type": "boolean", "difficulty": "easy",
+        "question": "The Python programming language gets its name from the British comedy group \"Monty Python.\"",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    }
+]
